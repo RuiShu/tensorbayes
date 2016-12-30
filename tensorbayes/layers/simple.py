569b98c3959a2a30078c5f044517592f87519735
Rui Shu <00ruishu@gmail.com>
2016-12-30 12:27:03 -0800

    Deprecate var clipping in GaussianUpdate

--- tensorbayes/layers/simple.py
+++ tensorbayes/layers/simple.py
@@ -11,9 +11,16 @@ def Dense(x, size, scope=None, activation=None, reuse=None):
                                              activation_fn=activation,
                                              reuse=reuse)
 
-def GaussianUpdate(zm1, zv1, zm2, zv2, scope=None):
+def GaussianUpdate(zm1, zv1, zm2, zv2, scope=None, eps=0.0):
     with tf.name_scope(scope):
         with tf.name_scope('variance'):
+            if eps > 0.0:
+                """It is not clear to me yet whether this will cause our loss function to be
+                severely biased
+                """
+                raise Exception("Adding eps noise deprecated at the moment for GaussianUpdate fn")
+                zv1 = tf.add(zv1, eps, name='clip_var1')
+                zv2 = tf.add(zv2, eps, name='clip_var2')
             zp1 = 1.0/zv1
             zp2 = 1.0/zv2
             zv = 1.0/(zp1 + zp2)
