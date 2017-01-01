d7df3f73b521327a6a7879d47ced1cc8c7a47a2d
Rui Shu <00ruishu@gmail.com>
2017-01-01 12:16:38 -0800

    Add importance weighting and monte carlo feature

--- tensorbayes/layers/sample.py
+++ tensorbayes/layers/sample.py
@@ -3,3 +3,16 @@ import tensorflow as tf
 def GaussianSample(mean, var, scope):
     with tf.name_scope(scope):
         return tf.random_normal(tf.shape(mean), mean, tf.sqrt(var))
+
+def Duplicate(x, n_iw=1, n_mc=1, scope=None):
+    """ Duplication function adds samples according to n_iw and n_mc.
+
+    This function is specifically for importance weighting and monte carlo
+    sampling.
+    """
+    with tf.name_scope(scope):
+        sample_shape = x._shape_as_list()[1:]
+        y = tf.reshape(x, [1, 1, -1] + sample_shape)
+        y = tf.tile(y, [n_iw, n_mc, 1] + [1] * len(sample_shape))
+        y = tf.reshape(y, [-1] + sample_shape)
+    return y
