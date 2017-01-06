ebdb9e2e21e9cc1bd0bcdaf735ec929cabe9c95d
Rui Shu <00ruishu@gmail.com>
2017-01-06 04:00:29 -0800

    Add gradient clipping fn

--- tensorbayes/tbutils.py
+++ tensorbayes/tbutils.py
@@ -6,8 +6,25 @@ def log_sum_exp(x, axis=1, keep_dims=False):
     if keep_dims:
         return out
     else:
-        return tf.squeeze(out, [axis])
+        if type(axis) is list:
+            return tf.squeeze(out, axis)
+        else:
+            return tf.squeeze(out, [axis])
 
 def cross_entropy_with_logits(logits, targets):
+    """
+    TODO: make this fn numerically stable.
+    """
     log_q = tf.nn.log_softmax(logits)
     return -tf.reduce_sum(targets * log_q, 1)
+
+def clip_gradients(optimizer, loss, max_clip=0.9, max_norm=4):
+    grads_and_vars = optimizer.compute_gradients(loss)
+    grads = [g for g, _ in grads_and_vars]
+    grads, global_grad_norm = tf.clip_by_global_norm(grads, max_norm)
+    clipped_grads_and_vars = []
+    for i in xrange(len(grads_and_vars)):
+        g = tf.clip_by_value(grads[i], -max_clip, max_clip)
+        v = grads_and_vars[i][1]
+        clipped_grads_and_vars += [(g, v)]
+    return clipped_grads_and_vars, global_grad_norm
