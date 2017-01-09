c54692bfb17cb6f90356056cd6311652692f0214
Rui Shu <00ruishu@gmail.com>
2017-01-09 02:52:08 -0800

    Add ability to return complementary set

--- tensorbayes/nputils.py
+++ tensorbayes/nputils.py
@@ -9,12 +9,14 @@ def kl_normal(qm, qv, pm, pv):
     return 0.5 * np.sum(np.log(pv) - np.log(qv) + qv/pv +
                         np.square(qm - pm) / pv - 1, axis=-1)
 
-def convert_to_ssl(x, y, n_labels, n_classes):
+def convert_to_ssl(x, y, n_labels, n_classes, complement=False):
     if y.shape[-1] == n_classes:
         y_sparse = y.argmax(1)
     else:
         y_sparse = y
     x_label, y_label = [], []
+    if complement:
+        x_comp, y_comp = [], []
     for i in xrange(n_classes):
         idx = y_sparse == i
         x_cand, y_cand = x[idx], y[idx]
@@ -22,9 +24,18 @@ def convert_to_ssl(x, y, n_labels, n_classes):
         x_select, y_select = x_cand[idx], y_cand[idx]
         x_label += [x_select]
         y_label += [y_select]
-    x_label = np.array(x_label).reshape(-1, *x.shape[1:])
-    y_label = np.array(y_label).reshape(-1, *y.shape[1:])
-    return x_label, y_label
+        if complement:
+            x_select, y_select = np.delete(x_cand, idx, 0), np.delete(y_cand, idx, 0)
+            x_comp += [x_select]
+            y_comp += [y_select]
+    x_label = np.concatenate(x_label, axis=0)
+    y_label = np.concatenate(y_label, axis=0)
+    if complement:
+        x_comp = np.concatenate(x_comp, axis=0)
+        y_comp = np.concatenate(y_comp, axis=0)
+        return x_label, y_label, x_comp, y_comp
+    else:
+        return x_label, y_label, x, y
 
 def conv_shape(x, k, s, p):
     if p == 'SAME':
