83c5cc34539f68360cbab585af9465e95f3ec592
Rui Shu <00ruishu@gmail.com>
2017-06-29 17:50:45 -0700

    Add nbutils import to base import

--- tensorbayes/__init__.py
+++ tensorbayes/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from . import layers
 from . import utils
 from . import nputils
@@ -5,3 +6,6 @@ from . import tbutils
 from . import distributions
 from .utils import FileWriter
 from .tbutils import function
+
+if 'ipykernel' in sys.argv[0]:
+    from . import nbutils
