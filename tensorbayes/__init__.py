d736589bc1244eafd9c1e83748fddd23e29d4482
rshu <rshu@ilcompm1.ilcomp>
2017-08-22 16:41:49 -0700

    Add TensorDict to name space. Add layers alias as nn

--- tensorbayes/__init__.py
+++ tensorbayes/__init__.py
@@ -1,11 +1,14 @@
 import sys
 from . import layers
+from . import layers as nn
 from . import utils
 from . import nputils
-from . import tbutils
+from . import tfutils
 from . import distributions
 from .utils import FileWriter
-from .tbutils import function
+from .tfutils import function, TensorDict
 
 if 'ipykernel' in sys.argv[0]:
     from . import nbutils
+
+__version__ = '0.2.0'
