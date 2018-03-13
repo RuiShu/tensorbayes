807aa2ec34441e919f27079eb4ed075f76fe17e5
Rui Shu <00ruishu@gmail.com>
2018-03-13 13:56:02 -0700

    Add log_bernoulli shortname

--- tensorbayes/distributions.py
+++ tensorbayes/distributions.py
@@ -3,13 +3,14 @@
 import tensorflow as tf
 import numpy as np
 
+def log_bernoulli(x, logits, eps=0.0, axis=-1):
+    return log_bernoulli_with_logits(x, logits, eps, axis)
+
 def log_bernoulli_with_logits(x, logits, eps=0.0, axis=-1):
     if eps > 0.0:
         max_val = np.log(1.0 - eps) - np.log(eps)
         logits = tf.clip_by_value(logits, -max_val, max_val,
                                   name='clipped_logit')
-    # return -tf.reduce_sum(
-    #     tf.nn.sigmoid_cross_entropy_with_logits(logits, x), axis)
     return -tf.reduce_sum(
         tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=x), axis)
 
